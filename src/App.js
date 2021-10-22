7a9c510035b319be34865bdc0fb3cd72c1ecf501
Reus <thdang0911@gmail.com>
2021-10-23 03:12:23 +0700

    creating heading (navbar)

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import Header from './components/Header'
+import "./reset.css"
 function App() {
-
   return (
     <div className="App">
       <Header title={"asdfsdf"}/>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,17 @@
+import './Header.css'
 
 const Header = (props) => {
     return (
-        <div>
-            {props.title}
-        </div>
+        <div className="navbar">
+            <div className="logo"><img src="images/logo_3svg.svg" alt="" /></div>
+            <ul>
+                <li><a href="">Menu</a></li>
+                <li><a href="">Job</a></li>
+                <li><a href="">Contact</a></li>
+                <li><a href="">About Us</a></li>
+                <li><a href="">Email</a></li>
+            </ul>
+        </div>      
     )
 }
 
