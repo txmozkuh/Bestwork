fdbd00cca57a75b00b81b01cf12fdbd2d3eb1350
Reus <thdang0911@gmail.com>
2021-10-28 02:26:47 +0700

    finished login page

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,11 @@
 import React from 'react'
 import "./Login.css"
-import TextField from '@mui/material/TextField';
-import { styled } from '@mui/material/styles';
+import PersonOutlineIcon from '@mui/icons-material/PersonOutline';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Checkbox from '@mui/material/Checkbox';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FacebookOutlinedIcon from '@mui/icons-material/FacebookOutlined';
+import GoogleIcon from '@mui/icons-material/Google';
 
 
 const Login = ({handleForm}) => {
@@ -12,47 +16,58 @@ const Login = ({handleForm}) => {
             handleForm(false,target)
         }
       }
-    
-    //<--Customize input field
-    const Input = styled(TextField)({
-        '& .MuiInputLabel-root':{
-            fontWeight:"bold",
-            textAlign:"center",
-            '&:hover':{
-                borderColor:"red"
-            }
-        },
-        '& label.Mui-focused': {
-          color: '#FF954A',
-        },
-        '& .MuiInput-underline:after': {
-          borderBottomColor: '#FF954A',
-        },
-        '& .MuiInput-root': {
-            color:"#FF954A",
-            font:"inherit",
-            fontWeight:"bold",
-          '& fieldset': {
-            borderColor: '#FF954A',
-          },
-          '&:hover fieldset': {
-            borderColor: '#FF954A',
-          },
-          '&.Mui-focused fieldset': {
-            borderColor: '#FF954A'
-          },
-        },
-      });
-
+    //<--Xử lí checkbox
+    const [checked, setChecked] = React.useState(true);
+    const handleChange = (event) => {
+      setChecked(event.target.checked);
+    };
     return (
         <div className="login_layer">
             <div className="login_container">
                 <div className="login_bg"><span className="title">WELCOM</span></div>
                 <div className="login_form">
-                    <span className="title">Sign In</span>
-                    <Input id="username" label="Username" variant="standard" color="secondary" margin="dense"/>
-                    <Input id="password" label="Password" variant="standard" color="secondary" margin="dense"/>
 
+                    <div className="title">Join Us</div>
+
+                    <div className="form">
+
+                        <div className="input_field">
+                            <PersonOutlineIcon color="disabled"/>
+                            <input type="text" name="account" id="account" placeholder="Username or Email" />
+                        </div>
+
+                        <div className="input_field">
+                            <LockOutlinedIcon color="disabled"/>
+                            <input type="password" name="password" id="password" placeholder="Password" />
+                        </div>
+
+                        <FormControlLabel 
+                            control={<Checkbox
+                                            checked={checked}
+                                            onChange={handleChange}
+                                            inputProps={{ 'aria-label': 'controlled' }}/>
+                                    }
+                            label="Remember password"
+                        />
+                        <button className="button">
+                            Sign in
+                        </button>
+                        <br /><br />
+                        <span style={{"color":"rgb(0,0,0,0.4)"}}>or login with</span>
+                        <div className="link_login">
+                            <div className="fb">
+                                <FacebookOutlinedIcon/>
+                                <img src="" alt="" />
+                                Facebook
+                            </div>
+                            <div className="gg">
+                                {/* <GoogleIcon/> */}
+                                <img src="./assets/images/icon_google.png" alt="" />
+                                Google
+                            </div>
+                        </div>
+                    </div>
+                    
                 </div>
             </div>
         </div>
