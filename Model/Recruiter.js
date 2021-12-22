71f3844aee297e5fddddae34aa480f8c2d2fd3bc
pmtoan <92153000+pmtoan@users.noreply.github.com>
2021-12-22 20:47:05 +0700

    Get list job, update job

--- Model/Recruiter.js
+++ Model/Recruiter.js
@@ -11,9 +11,7 @@ exports.getProfile = async (req, res) => {
                                          WHERE Recruiter_ID='${recruiter_id}'`);
     const profile = result.recordset[0];
 
-    return {
-        profile
-    }
+    return profile;
 }
 
 exports.updateProfile = async (req, res) => {
@@ -44,10 +42,10 @@ exports.createJob = async (req, res) => {
     //-------------------------- Recruiter_Job --------------------------------\\
     sqlString = `INSERT INTO Recruiter_Job (Recruiter_ID, Job_Name, Salary, Start_Date, End_date, District,
                                                 City, Working_Form, Recruitment_Quantity, Status, Remote,
-                                                Years_Of_Experient, Type_ID) 
+                                                Years_Of_Experience, Type_ID) 
                     VALUES(${user.recruiter_id}, N'${body['job-name']}', '${body.salary}', '${body['start-date']}',
                      '${body['end-date']}', N'${body.district}', N'${body.city}', N'${body['working-form']}', 
-                     '${body['recruitment_quantity']}', N'pending', '${body.remote}', '${body['years-of-experient']}',
+                     '${body['recruitment_quantity']}', N'pending', '${body.remote}', '${body['years-of-experience']}',
                      '${body['type-id']}')
                     SELECT SCOPE_IDENTITY() AS Job_ID`;
 
@@ -62,10 +60,18 @@ exports.createJob = async (req, res) => {
 }
 
 exports.getCreatedJob = async (req, res) => {
-    const job_id = req.params.jobId;
     const pool = await connect;
     const request = pool.request();
     let result =  null;
+
+    result = await request.query(`SELECT Recruiter.Recruiter_Name, Recruiter_Job.Job_Name, Recruiter_Job.District,
+                                        Recruiter_Job.city, Recruiter_Job.Salary, Recruiter_Job.Start_Date, 
+                                        Recruiter_Job.End_Date, Recruiter_Job.Status
+                                  FROM Recruiter_Job join Recruiter on Recruiter_Job.Recruiter_ID = Recruiter.Recruiter_ID
+                                  WHERE Recruiter.Recruiter_ID='${req.user.recruiter_id}'`);
+    const job = result.recordset;
+
+    return job;
 }
 
 exports.getJobDescription = async (req, res) => {
@@ -92,14 +98,38 @@ exports.getJobDescription = async (req, res) => {
                                   WHERE Recruiter_Job_ID='${description.Recruiter_Job_ID}'`);
     const experience_require = result.recordset;
 
-    return {
-        description,
-        job_type,
-        experience_require
-    }
+    return {description, job_type, experience_require}
+
 }
 
 exports.updateJob = async (req, res) => {
+    const job_id = req.params.jobId;
+    const body = req.body;
+
+    const pool = await connect;
+    const request = pool.request();
+
+    await request.query(`UPDATE Recruiter_Job 
+                         SET Job_Name = N'${body['job-name']}',
+                             Salary = '${body.salary}',
+                             Start_Date = '${body['start-date']}',
+                             End_Date = '${body['end-date']}',
+                             District = N'${body.district}',
+                             City = N'${body.city}',
+                             Working_Form = N'${body['working-form']}',
+                             Recruitment_Quantity = '${body['recruitment_quantity']}',
+                             Remote = '${body.remote}',
+                             Years_Of_Experience = '${body['years-of-experience']}',
+                             Type_ID = '${body['type-id']}'
+                         WHERE Recruiter_Job_ID = '${job_id}'`);
+
+    await request.query(`DELETE FROM Experience_Require 
+                         WHERE Recruiter_Job_ID = '${job_id}'`);
+
+    for(let i = 0; i < body['skill-id'].length; i++){
+        await request.query(`INSERT INTO Experience_Require (Recruiter_Job_ID, Skill_ID)
+                                        VALUES('${job_id}', '${body['skill-id'][i]}')`);
+    }
 
 }
 
