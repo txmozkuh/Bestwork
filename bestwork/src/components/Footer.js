7b128962cd532e12638dad47753ba445b360afcd
Reus <thdang0911@gmail.com>
2021-12-26 18:30:31 +0700

    complete footer

--- bestwork/src/components/Footer.js
+++ bestwork/src/components/Footer.js
@@ -1,11 +1,115 @@
-import React from 'react'
-import './css/Footer.css'
-const Footer = () => {
-    return (
-        <div className="footer_container">
-            <h1>Footer</h1>
-        </div>
-    )
-}
+import React from "react";
+import styled from 'styled-components';
 
-export default Footer
+const Footer = () => {
+  return (
+    <Box>
+      <Container>
+        <Row>
+          <Column>
+            <Heading>About Us</Heading>
+            <FooterLink href="#">Aim</FooterLink>
+            <FooterLink href="#">Vision</FooterLink>
+            <FooterLink href="#">Testimonials</FooterLink>
+          </Column>
+          <Column>
+            <Heading>Services</Heading>
+            <FooterLink href="#">Job</FooterLink>
+            <FooterLink href="#">Internships</FooterLink>
+            <FooterLink href="#">Connect</FooterLink>
+            <FooterLink href="#">Cooperate</FooterLink>
+          </Column>
+          <Column>
+            <Heading>Contact Us</Heading>
+            <FooterLink href="#">Uttar Pradesh</FooterLink>
+            <FooterLink href="#">Ahemdabad</FooterLink>
+            <FooterLink href="#">Indore</FooterLink>
+            <FooterLink href="#">Mumbai</FooterLink>
+          </Column>
+          <Column>
+            <Heading>Social Media</Heading>
+            <FooterLink href="#">
+                <span style={{ marginLeft: "10px" }}>
+                  Facebook
+                </span>
+            </FooterLink>
+            <FooterLink href="#">
+                <span style={{ marginLeft: "10px" }}>
+                  Instagram
+                </span>
+              
+            </FooterLink>
+            <FooterLink href="#">
+                <span style={{ marginLeft: "10px" }}>
+                  Twitter
+                </span>
+            </FooterLink>
+            <FooterLink href="#">
+                <span style={{ marginLeft: "10px" }}>
+                  Youtube
+                </span>
+            </FooterLink>
+          </Column>
+        </Row>
+      </Container>
+    </Box>
+  );
+};
+export default Footer;
+
+export const Box = styled.div`
+  padding: 80px 60px;
+  background:#4d4d4d;
+  width: 100%;
+  
+  @media (max-width: 1000px) {
+    padding: 70px 30px;
+  }
+`;
+   
+export const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    max-width: 1000px;
+    margin: 0 auto;
+    /* background: red; */
+`
+   
+export const Column = styled.div`
+  display: flex;
+  flex-direction: column;
+  text-align: left;
+  margin-left: 60px;
+`;
+   
+export const Row = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, 
+                         minmax(185px, 1fr));
+  grid-gap: 20px;
+   
+  @media (max-width: 1000px) {
+    grid-template-columns: repeat(auto-fill, 
+                           minmax(200px, 1fr));
+  }
+`;
+   
+export const FooterLink = styled.a`
+  color: #fff;
+  margin-bottom: 20px;
+  font-size: 18px;
+  text-decoration: none;
+   
+  &:hover {
+      color: rgb(238,125,52);
+      transition: 200ms ease-in;
+  }
+`;
+   
+export const Heading = styled.p`
+  font-size: 24px;
+  color: #fff;
+  margin-bottom: 40px;
+  font-weight: bold;
+`;
