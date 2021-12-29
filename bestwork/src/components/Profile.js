3aacd534c293dff82dc0bf85270f08cf401aa614
Reus <thdang0911@gmail.com>
2021-12-29 14:08:53 +0700

    update form

--- bestwork/src/components/Profile.js
+++ bestwork/src/components/Profile.js
@@ -14,9 +14,7 @@ const Profile = () => {
             setInfo(res.data.candidate.profile)
         })
     }, [])
-    const handleUpdateProfile = () => {
-        
-    }
+    
     return (
         <>
         {
@@ -32,10 +30,19 @@ const Profile = () => {
 export default Profile
 
 export const TableInfo = (props) =>{
-
+    let update=false
+    const handleUpdateProfile = () => {
+        update=!update
+        console.log(update)
+    }
+    
     return (
         <div className='profile_container'>
-                    <table class="styled-table">
+                    {
+                        update===true?
+                        <UpdateForm/>
+                        :
+                        <table class="styled-table">
                         <thead>
                             <tr>
                                 <th>Thông tin cá nhân</th>
@@ -70,9 +77,55 @@ export const TableInfo = (props) =>{
                             </tr>
                         </tbody>
                     </table>
+                    }
+                    <div className="button" onClick={handleUpdateProfile}>
+                        Update profile
+                    </div>
+        </div>
+    )
+}
+
+export const UpdateForm = () => {
+    return(
+        <div className='profile_container'>
+                    <table class="styled-table">
+                        <thead>
+                            <tr>
+                                <th>Thông tin cá nhân</th>
+                                <th>asda</th>
+                                input 
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>Email</td>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <td>Họ và tên</td>
+                                <td class='word_color'>ví dụ: Nguyễn Văn A</td>
+                            </tr>
+                            <tr>
+                                <td>Điện thoại</td>
+                                <td class='word_color'>ví dụ: 0908123456</td>
+                            </tr>
+                            <tr>
+                                <td>Ngày sinh</td>
+                                <td class='word_color'>ví dụ: 01/01/2001</td>
+                            </tr>
+                            <tr>
+                                <td>Giới tính</td>
+                                <td class='word_color'>ví dụ: Nam</td>
+                            </tr>
+                            <tr>
+                                <td>Về bản thân</td>
+                                <td class='word_color'>ví dụ: Là một người thích code...</td>
+                            </tr>
+                        </tbody>
+                    </table>
                     <div className="button">
                         Update profile
                     </div>
-                </div>
+        </div>
     )
-}
+} 
