6529431a4970edd7cd87644f4aa28449f3346dae
Reus <thdang0911@gmail.com>
2021-12-29 13:56:50 +0700

    handle load profile data

--- bestwork/src/components/Profile.js
+++ bestwork/src/components/Profile.js
@@ -2,67 +2,77 @@ import React from 'react'
 import './css/Profile.css'
 import axios from 'axios'
 import {useParams} from 'react-router-dom'
-
-
-
+import CircularProgress from '@mui/material/CircularProgress';
 
 const Profile = () => {
-    const [info,setInfo] = React.useState([])
-
+    const [UserInfo,setInfo] = React.useState({})
     React.useEffect(() => {
         axios.get('http://localhost:3001/candidate/profile',
         {
             withCredentials: true
         }).then((res)=>{
-            setInfo(res.data)
+            setInfo(res.data.candidate.profile)
         })
     }, [])
-    const profile = info.candidate
     const handleUpdateProfile = () => {
         
     }
     return (
-        
-        <div className='profile_container'>
-            <table class="styled-table">
-                <thead>
-                    <tr>
-                        <th>Thông tin cá nhân</th>
-                        <th></th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>Email</td>
-                        <td>{console.log(profile)}</td>
-                    </tr>
-                    <tr>
-                        <td>Họ và tên</td>
-                        <td class='word_color'>ví dụ: Nguyễn Văn A</td>
-                    </tr>
-                    <tr>
-                        <td>Điện thoại</td>
-                        <td class='word_color'>ví dụ: 0908123456</td>
-                    </tr>
-                    <tr>
-                        <td>Ngày sinh</td>
-                        <td class='word_color'>ví dụ: 01/01/2001</td>
-                    </tr>
-                    <tr>
-                        <td>Giới tính</td>
-                        <td class='word_color'>ví dụ: Nam</td>
-                    </tr>
-                    <tr>
-                        <td>Về bản thân</td>
-                        <td class='word_color'>ví dụ: Là một người thích code...</td>
-                    </tr>
-                </tbody>
-            </table>
-            <div className="button" onClick={handleUpdateProfile}>
-                Update profile
-            </div>
-        </div>
+        <>
+        {
+            Object.keys(UserInfo).length === 0 && UserInfo.constructor === Object?
+            <div className='profile_container'><CircularProgress/></div>
+            :
+            <TableInfo info={UserInfo} />
+        }
+        </>
     )
 }
 
 export default Profile
+
+export const TableInfo = (props) =>{
+
+    return (
+        <div className='profile_container'>
+                    <table class="styled-table">
+                        <thead>
+                            <tr>
+                                <th>Thông tin cá nhân</th>
+                                <th>{props.info.Email}</th>
+                                {console.log(props.info)}
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>Email</td>
+                                <td>{props.info.Account_ID}</td>
+                            </tr>
+                            <tr>
+                                <td>Họ và tên</td>
+                                <td class='word_color'>ví dụ: Nguyễn Văn A</td>
+                            </tr>
+                            <tr>
+                                <td>Điện thoại</td>
+                                <td class='word_color'>ví dụ: 0908123456</td>
+                            </tr>
+                            <tr>
+                                <td>Ngày sinh</td>
+                                <td class='word_color'>ví dụ: 01/01/2001</td>
+                            </tr>
+                            <tr>
+                                <td>Giới tính</td>
+                                <td class='word_color'>ví dụ: Nam</td>
+                            </tr>
+                            <tr>
+                                <td>Về bản thân</td>
+                                <td class='word_color'>ví dụ: Là một người thích code...</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                    <div className="button">
+                        Update profile
+                    </div>
+                </div>
+    )
+}
